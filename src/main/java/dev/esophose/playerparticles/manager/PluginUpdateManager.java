12f68b738175f63af7aa01d4cc298873fd84f769
Esophose <esophose@gmail.com>
2020-01-26 04:47:09 -0700

    Update the updater

--- src/main/java/dev/esophose/playerparticles/manager/PluginUpdateManager.java
+++ src/main/java/dev/esophose/playerparticles/manager/PluginUpdateManager.java
@@ -3,7 +3,12 @@ package dev.esophose.playerparticles.manager;
 import dev.esophose.playerparticles.PlayerParticles;
 import dev.esophose.playerparticles.manager.ConfigurationManager.Setting;
 import dev.esophose.playerparticles.util.StringPlaceholders;
-import dev.esophose.playerparticles.util.Updater;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,14 +28,16 @@ public class PluginUpdateManager extends Manager implements Listener {
     public void reload() {
         if (Setting.CHECK_UPDATES.getBoolean()) {
             Bukkit.getScheduler().runTaskAsynchronously(this.playerParticles, () -> {
-                try { // This can throw an exception if the server has no internet connection or if the Curse API is down
-                    Updater updater = new Updater(this.playerParticles, 82823, this.playerParticles.getJarFile(), Updater.UpdateType.NO_DOWNLOAD, false);
-                    if (Double.parseDouble(updater.getLatestName().replaceAll("PlayerParticles v", "")) > Double.parseDouble(this.playerParticles.getDescription().getVersion())) {
-                        this.updateVersion = updater.getLatestName().replaceAll("PlayerParticles v", "");
-                        this.playerParticles.getLogger().info("An update (v" + this.updateVersion + ") is available! You are running v" + this.playerParticles.getDescription().getVersion());
+                try {
+                    String latestVersion = this.getLatestVersion();
+                    String currentVersion = this.playerParticles.getDescription().getVersion();
+
+                    if (this.isUpdateAvailable(latestVersion, currentVersion)) {
+                        this.updateVersion = latestVersion;
+                        this.playerParticles.getLogger().info("An update (v" + this.updateVersion + ") is available! You are running v" + currentVersion);
                     }
                 } catch (Exception e) {
-                    this.playerParticles.getLogger().warning("An error occurred checking for an update. There is either no established internet connection or the Curse API is down.");
+                    this.playerParticles.getLogger().warning("An error occurred checking for an update. There is either no established internet connection or the Spigot API is down.");
                 }
             });
         }
@@ -42,13 +49,53 @@ public class PluginUpdateManager extends Manager implements Listener {
     }
 
     /**
+     * Checks if there is an update available
+     *
+     * @param latest The latest version of the plugin from Spigot
+     * @param current The currently installed version of the plugin
+     * @return true if available, otherwise false
+     */
+    private boolean isUpdateAvailable(String latest, String current) {
+        // Break versions into individual numerical pieces separated by periods
+        int[] latestSplit = Arrays.stream(latest.replaceAll("[^0-9.]", "").split(Pattern.quote("."))).mapToInt(Integer::parseInt).toArray();
+        int[] currentSplit = Arrays.stream(current.replaceAll("[^0-9.]", "").split(Pattern.quote("."))).mapToInt(Integer::parseInt).toArray();
+
+        // Make sure both arrays are the same length
+        if (latestSplit.length > currentSplit.length) {
+            currentSplit = Arrays.copyOf(currentSplit, latestSplit.length);
+        } else if (currentSplit.length > latestSplit.length) {
+            latestSplit = Arrays.copyOf(latestSplit, currentSplit.length);
+        }
+
+        // Compare pieces from most significant to least significant
+        for (int i = 0; i < latestSplit.length; i++)
+            if (latestSplit[i] > currentSplit[i])
+                return true;
+
+        return false;
+    }
+
+    /**
+     * Gets the latest version of the plugin from the Spigot Web API
+     *
+     * @return the latest version of the plugin from Spigot
+     * @throws IOException if a network error occurs
+     */
+    private String getLatestVersion() throws IOException {
+        URL spigot = new URL("https://api.spigotmc.org/legacy/update.php?resource=40261");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(spigot.openStream()))) {
+            return reader.readLine();
+        }
+    }
+
+    /**
      * Called when a player joins and notifies ops if an update is available
      *
      * @param e The join event
      */
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        if (e.getPlayer().isOp() && this.updateVersion != null) {
+        if (this.updateVersion != null && e.getPlayer().isOp()) {
             PlayerParticles.getInstance().getManager(LocaleManager.class).sendMessage(
                     e.getPlayer(),
                     "update-available",
